cb5a6b4ab4de75c991c6fc584c8dd009fe0ca797
shaun <ycorredius@gmail.com>
2020-04-11 13:17:19 -0400

    Done.

--- src/Comment.js
+++ src/Comment.js
@@ -1,1 +1,12 @@
 //your code here
+import React, {Component} from 'react';
+import Blogspot from './BlogPost';
+
+export default class Comment extends Component{
+    render(){
+        return(
+        <div className= "comment">
+            {this.props.commentText}
+        </div>)
+    };
+}
